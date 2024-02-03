5c45530ef16c935f4e979d90dafe353bfba844af
mferdiyanzah <mferdiyanzah@gmail.com>
2024-02-03 14:52:29 +0700

    feat: add function to add and delete item

---
+++ index.js
@@ -0,0 +1,42 @@
+let totalItems = 0;
+
+const cartEl = document.getElementById("cart");
+
+const descriptionEl = document.createElement("p");
+cartEl.appendChild(descriptionEl);
+
+const checkTotal = () => {
+  if (totalItems === 0) {
+    descriptionEl.innerHTML = "Keranjang belanja masih kosong";
+  } else {
+    descriptionEl.innerHTML = `Total Jenis Barang: ${totalItems}`;
+  }
+};
+checkTotal();
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const itemName = document.getElementById("item_name").value;
+  const itemTotal = document.getElementById("item_total").value;
+
+  const itemEl = document.createElement("li");
+
+  const textEl = document.createElement("span");
+  textEl.innerHTML = `${itemName} - ${itemTotal} barang`;
+  itemEl.appendChild(textEl);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "Delete";
+  deleteBtn.onclick = () => {
+    itemEl.remove();
+  };
+  itemEl.appendChild(deleteBtn);
+
+  cartEl.appendChild(itemEl);
+
+  totalItems++;
+  checkTotal();
+};
+
+const formEl = document.getElementById("add-form");
+formEl.addEventListener("submit", onSubmit);
